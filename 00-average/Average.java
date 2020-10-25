16fc4c390331f2dd6faddf7594729aa4dd1fbb51
yanglu88 <yanglu91603@gmail.com>
2020-10-25 16:18:31 -0400

    added for loop

--- 00-average/Average.java
+++ 00-average/Average.java
@@ -4,25 +4,14 @@ public class Average{
     Scanner input = new Scanner(System.in);
     double sum = 0;
 
-    System.out.print("Enter your first grade:");
-    sum += input.nextInt();
-
-    System.out.print("Enter your second grade:");
-    sum += input.nextInt();
-
-    System.out.print("Enter your third grade:");
-    sum += input.nextInt();
-
-    System.out.print("Enter your fourth grade:");
-    sum += input.nextInt();
-
-    System.out.print("Enter your fifth grade:");
-    sum += input.nextInt();
+    for (int i = 0; i < 5; i++){
+      System.out.print("Enter your grades: ");
+      sum += input.nextInt();
+    }//end for loop
 
     double Average = (double) sum / 5;
 
     System.out.println(Average);
 
-
-  }
-}
+  }//end main method
+}//end class
