a930629ce51a62f9749b1ef3959e651c6158ed02
yanglu88 <yanglu91603@gmail.com>
2020-10-26 15:47:10 -0400

    allows input to be doubles

--- 06-math/Distance.java
+++ 06-math/Distance.java
@@ -2,31 +2,28 @@ import java.util.Scanner;
 public class Distance{
   public static void main(String[] args){
     Scanner input = new Scanner(System.in);
-    int x1 = 0;
-    int x2 = 0;
-    int y1 = 0;
-    int y2 = 0;
+    double x1, x2, y1, y2;
 
     System.out.print("What is the x coordinate: ");
-    x1 = input.nextInt();
-    System.out.print("What is the y coodrinate: ");
-    y1 = input.nextInt();
+    x1 = input.nextDouble();
+    System.out.print("What is the y coordinate: ");
+    y1 = input.nextDouble();
     System.out.print("What is the new x coordinate: ");
-    x2 = input.nextInt();
+    x2 = input.nextDouble();
     System.out.print("What is the new y coordinate: ");
-    y2 = input.nextInt();
+    y2 = input.nextDouble();
 
     double distance = Math.sqrt((x2 - x1)*(x2 - x1) + (y2 - y1)*(y2-y1));
     System.out.println("The distance is: " + distance);
 
     System.out.print("What is the x coordinate: ");
-    x1 = input.nextInt();
+    x1 = input.nextDouble();
     System.out.print("What is the y coodrinate: ");
-    y1 = input.nextInt();
+    y1 = input.nextDouble();
     System.out.print("What is the new x coordinate: ");
-    x2 = input.nextInt();
+    x2 = input.nextDouble();
     System.out.print("What is the new y coordinate: ");
-    y2 = input.nextInt();
+    y2 = input.nextDouble();
 
     double taxicabDistance = Math.abs(x2 - x1) + Math.abs(y2 - y1);
     System.out.print("The taxicabDistance is: " + taxicabDistance);
