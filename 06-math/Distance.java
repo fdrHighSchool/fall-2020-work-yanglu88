a7678aab0043b2a1eec0501b9bb6de3151ca946f
yanglu88 <yanglu91603@gmail.com>
2020-10-28 16:15:15 -0400

    added string methods

--- 06-math/Distance.java
+++ 06-math/Distance.java
@@ -1,34 +1,63 @@
 import java.util.Scanner;
 public class Distance{
   public static void main(String[] args){
-    Scanner input = new Scanner(System.in);
-    double x1, x2, y1, y2;
+    cartesianDistance();
+    taxicabDistance();
 
-    System.out.print("What is the x coordinate: ");
-    x1 = input.nextDouble();
-    System.out.print("What is the y coordinate: ");
-    y1 = input.nextDouble();
-    System.out.print("What is the new x coordinate: ");
-    x2 = input.nextDouble();
-    System.out.print("What is the new y coordinate: ");
-    y2 = input.nextDouble();
+    }//end main method
+
+  public static void cartesianDistance(){
+    Scanner input = new Scanner(System.in);
+
+    System.out.print("Enter the first point: ");
+    String p1 = input.nextLine();
+
+    System.out.print("Enter the second point: ");
+    String p2 = input.nextLine();
+
+    double x1 = getX(p1);
+    double x2 = getX(p2);
+    double y1 = getY(p1);
+    double y2 = getY(p2);
 
     double distance = Math.sqrt((x2 - x1)*(x2 - x1) + (y2 - y1)*(y2-y1));
     System.out.println("The distance is: " + distance);
 
-    System.out.print("What is the x coordinate: ");
-    x1 = input.nextDouble();
-    System.out.print("What is the y coodrinate: ");
-    y1 = input.nextDouble();
-    System.out.print("What is the new x coordinate: ");
-    x2 = input.nextDouble();
-    System.out.print("What is the new y coordinate: ");
-    y2 = input.nextDouble();
+  }//end cartesianDistance
+
+
+  public static void taxicabDistance(){
+    Scanner input = new Scanner(System.in);
+
+    System.out.print("Enter the first point: ");
+    String p1 = input.nextLine();
+
+    System.out.print("Enter the second point: ");
+    String p2 = input.nextLine();
+
+    double x1 = getX(p1);
+    double x2 = getX(p2);
+    double y1 = getY(p1);
+    double y2 = getY(p2);
 
     double taxicabDistance = Math.abs(x2 - x1) + Math.abs(y2 - y1);
-    System.out.print("The taxicabDistance is: " + taxicabDistance);
+    System.out.print("The taxicab distance is: " + taxicabDistance);
 
-  }//end main method
+  }//end taxicabDistance
+
+  public static int getX(String point){
+    int comma = point.indexOf(",");
+    String x = point.substring(1, comma);
+
+    return Integer.parseInt(x);
+  }//end getX
+
+  public static int getY(String point){
+    int comma = point.indexOf(",");
+    String y = point.substring(comma + 2 , point.length()-1);
+
+    return Integer.parseInt(y);
+  }//end getY
 
 
 }//end class
